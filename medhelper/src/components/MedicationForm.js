b6d4ab155943072131019052356bebe67aab9bbb
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-04-12 00:30:10 +0100

    new ver of the medication reminder

--- medhelper/src/components/MedicationForm.js
+++ medhelper/src/components/MedicationForm.js
@@ -3,51 +3,71 @@ import React, { useState } from 'react';
 
 function MedicationForm({ onSave, onCancel }) {
     const [name, setName] = useState('');
-    const [dosage, setDosage] = useState('');
+    const [description, setDescription] = useState('');
     const [frequency, setFrequency] = useState('');
+    const [timesPerDay, setTimesPerDay] = useState('');
     const [time, setTime] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        onSave({ name, dosage, frequency, time });
+        onSave({ name, description, frequency, timesPerDay, time });
         setName('');
-        setDosage('');
+        setDescription('');
         setFrequency('');
+        setTimesPerDay('');
         setTime('');
     };
 
     return (
-        <form className="add-reminder-form" onSubmit={handleSubmit}>
+        <form className="add-medication-form" onSubmit={handleSubmit}>
             <div>
-                <label htmlFor="name">Name</label>
+                <label htmlFor="name">Medication Name</label>
                 <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} />
             </div>
             <div>
-                <label htmlFor="dosage">Dosage</label>
-                <input type="text" id="dosage" value={dosage} onChange={(event) => setDosage(event.target.value)} />
+                <label htmlFor="description">Dosage/Description</label>
+                <input type="text" id="description" value={description} onChange={(event) => setDescription(event.target.value)} />
             </div>
             <div>
                 <label htmlFor="frequency">Frequency</label>
                 <select id="frequency" value={frequency} onChange={(event) => setFrequency(event.target.value)}>
                     <option value="">-- Select Frequency --</option>
-                    <option value="Daily">Daily</option>
-                    <option value="Twice daily">Twice daily</option>
-                    <option value="Weekly">Weekly</option>
-                    <option value="Monthly">Monthly</option>
+                    <option value="daily">Daily</option>
+                    <option value="hourly">Every x hours</option>
+                    <option value="x-times-a-day">X times a day</option>
+                    <option value="weekly">Weekly</option>
+                    <option value="monthly">Monthly</option>
                 </select>
             </div>
-            <div>
-                <label htmlFor="time">Time</label>
-                <input type="time" id="time" value={time} onChange={(event) => setTime(event.target.value)} />
-            </div>
-            <div className="button-container">
-                <button type="submit">Save</button>
-                <button type="button" onClick={onCancel}>Cancel</button>
+            {frequency === 'hourly' && (
+                <div>
+                    <label htmlFor="timesPerDay">Every x hours</label>
+                    <input type="number" id="timesPerDay" value={timesPerDay} onChange={(event) => setTimesPerDay(event.target.value)} />
+                </div>
+            )}
+            {frequency === 'x-times-a-day' && (
+                <div>
+                    <label htmlFor="timesPerDay">X times a day</label>
+                    <input type="number" id="timesPerDay" value={timesPerDay} onChange={(event) => setTimesPerDay(event.target.value)} />
+                </div>
+            )}
+            {(frequency === 'daily' || frequency === 'hourly' || frequency === 'x-times-a-day') && (
+                <div>
+                    <label htmlFor="time">Time</label>
+                    <input type="time" id="time" value={time} onChange={(event) => setTime(event.target.value)} />
+                    {frequency === 'x-times-a-day' && <p className="hint">Enter times in 24-hour format (e.g. 08:00, 13:00, 20:00)</p>}
+                </div>
+            )}
+            <div className="button-group">
+                <button type="submit" className="btn-primary">
+                    Add Medication
+                </button>
+                <button type="button" className="btn-secondary" onClick={onCancel}>
+                    Cancel
+                </button>
             </div>
         </form>
     );
 }
 
 export default MedicationForm;
-
-
--- medhelper/src/components/MedicationList.js
+++ medhelper/src/components/MedicationList.js
@@ -1,11 +1,21 @@
-import React from 'react';
 import Medication from './Medication';
 
 function MedicationList({ medications, toggleTaken }) {
     return (
         <ul className="medication-list">
-            {medications.map(medication => (
-                <Medication key={medication.id} medication={medication} toggleTaken={toggleTaken} />
+            <li className="header">
+                <p className="name">Name</p>
+                <p className="dosage">Dosage</p>
+                <p className="frequency">Frequency</p>
+                <p className="time">Time</p>
+                <p className="taken">Taken</p>
+            </li>
+            {medications.map((medication) => (
+                <Medication
+                    key={medication.id}
+                    medication={medication}
+                    toggleTaken={toggleTaken}
+                />
             ))}
         </ul>
     );
