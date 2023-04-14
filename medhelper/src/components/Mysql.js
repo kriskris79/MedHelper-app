a3ddd4ce2b7e15c0504d752f3d90806fd586d841
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-04-14 16:24:56 +0100

    gitignore

---
+++ medhelper/src/components/Mysql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+require('dotenv').config();
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL server!');
+});
+
+connection.query('SELECT * FROM your_table', (err, results) => {
+    if (err) throw err;
+    console.log(results);
+});
+
+connection.end();
