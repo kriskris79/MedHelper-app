8f2f0439536702ee716f36db0d66419a894683b0
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-04-17 15:27:26 +0100

    working veriosn

--- medhelper/src/components/Mysql.js
+++ medhelper/src/components/Mysql.js
@@ -1,20 +1,28 @@
 const mysql = require('mysql');
 require('dotenv').config();
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error('Error connecting to MySQL database: ', err);
+        return;
+    }
+
     console.log('Connected to MySQL server!');
-});
 
-connection.query('SELECT * FROM medication2', (err, results) => {
-    if (err) throw err;
-    console.log(results);
-});
+    connection.query('SELECT * FROM medication2', (err, results) => {
+        if (err) {
+            console.error('Error querying database: ', err);
+            return;
+        }
 
-connection.end();
+        console.log(results);
+        connection.end();
+    });
+});
