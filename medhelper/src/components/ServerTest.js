7b5d55eb288d7fcc501f7e9b68f51151ee9c8c65
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-04-19 13:28:15 +0100

    before CORS

--- medhelper/src/components/ServerTest.js
+++ medhelper/src/components/ServerTest.js
@@ -95,6 +95,10 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 connection.connect((error) => {
     if (error) {
