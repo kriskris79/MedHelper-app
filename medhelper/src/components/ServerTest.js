4cea83e213597051749b09411d921f7af891bc14
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-04-19 11:12:27 +0100

    test with JWT aut

--- medhelper/src/components/ServerTest.js
+++ medhelper/src/components/ServerTest.js
@@ -1,3 +1,83 @@
+//
+// require('dotenv').config();
+// const express = require('express');
+// const mysql = require('mysql');
+// const jwt = require('jsonwebtoken');
+//
+// const app = express();
+// const secret = 'secret-key';
+//
+// app.use(express.json());
+//
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+//
+// connection.connect((error) => {
+//     if (error) {
+//         console.error(`Error connecting to database: ${error}`);
+//         return;
+//     }
+//
+//     console.log('Connected to database');
+// });
+//
+// const generateToken = (user) => {
+//     return jwt.sign({ user }, secret, { expiresIn: '365d' });
+// };
+//
+// const verifyToken = (token) => {
+//     return jwt.verify(token, secret);
+// };
+//
+// const authenticate = (req, res, next) => {
+//     const authorizationHeader = req.headers.authorization;
+//
+//     if (!authorizationHeader) {
+//         return res.status(401).send({ message: 'Unauthorized' });
+//     }
+//
+//     const token = authorizationHeader.split(' ')[1];
+//
+//     try {
+//         req.user = verifyToken(token);
+//         next();
+//     } catch (error) {
+//         return res.status(401).send({ message: 'Unauthorized' });
+//     }
+// };
+//
+// app.post('/api/medication2', authenticate, (req, res) => {
+//     const sql = `INSERT INTO medication2 (name, description, frequency, times, dosage) VALUES ?`;
+//     const values = [
+//         [req.body.name, req.body.description, req.body.frequency, req.body.times, req.body.dosage]
+//     ];
+//
+//     connection.query(sql, [values], (error, results) => {
+//         if (error) {
+//             console.error(`Error saving medication: ${error}`);
+//             return res.status(500).send({ message: 'Error saving medication' });
+//         }
+//
+//         res.status(201).send({ message: 'Medication saved successfully' });
+//     });
+// });
+//
+// app.post('/api/reset-token', (req, res) => {
+//     const user = req.body.user;
+//     const newToken = generateToken(user);
+//
+//     // Store the new token in a database or other persistent storage
+//
+//     res.status(200).send({ message: 'Token reset successful', newToken });
+// });
+//
+// app.listen(3000, () => {
+//     console.log('Server started on port 3000');
+// });
 
 require('dotenv').config();
 const express = require('express');
@@ -25,8 +105,8 @@ connection.connect((error) => {
     console.log('Connected to database');
 });
 
-const generateToken = (user) => {
-    return jwt.sign({ user }, secret, { expiresIn: '365d' });
+const generateToken = (ip) => {
+    return jwt.sign({ ip }, secret, { expiresIn: '365d' });
 };
 
 const verifyToken = (token) => {
@@ -43,7 +123,7 @@ const authenticate = (req, res, next) => {
     const token = authorizationHeader.split(' ')[1];
 
     try {
-        req.user = verifyToken(token);
+        req.ip = verifyToken(token).ip;
         next();
     } catch (error) {
         return res.status(401).send({ message: 'Unauthorized' });
@@ -51,9 +131,9 @@ const authenticate = (req, res, next) => {
 };
 
 app.post('/api/medication2', authenticate, (req, res) => {
-    const sql = `INSERT INTO medication2 (name, description, frequency, times, dosage) VALUES ?`;
+    const sql = `INSERT INTO medication2 (name, description, frequency, times, dosage, ip) VALUES ?`;
     const values = [
-        [req.body.name, req.body.description, req.body.frequency, req.body.times, req.body.dosage]
+        [req.body.name, req.body.description, req.body.frequency, req.body.times, req.body.dosage, req.ip]
     ];
 
     connection.query(sql, [values], (error, results) => {
@@ -67,10 +147,8 @@ app.post('/api/medication2', authenticate, (req, res) => {
 });
 
 app.post('/api/reset-token', (req, res) => {
-    const user = req.body.user;
-    const newToken = generateToken(user);
-
-    // Store the new token in a database or other persistent storage
+    const ip = req.ip;
+    const newToken = generateToken(ip);
 
     res.status(200).send({ message: 'Token reset successful', newToken });
 });
