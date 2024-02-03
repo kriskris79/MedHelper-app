57d7404c18a70b09d07fd876722c0ca7ea9ef484
kriskris79 <krzysztofkakolewski@googlemail.com>
2024-02-03 22:23:11 +0000

    its working now

--- medhelper/src/components/Medication.js
+++ medhelper/src/components/Medication.js
@@ -8,9 +8,9 @@ function Medication({ medication, toggleTaken, onDelete }) {
         displayFrequency = `${times.length} times a day`;
     }
 
-    const handleTakenChange = (index) => {
-        toggleTaken(id,index); // need to handel an index
-    };
+    // const handleTakenChange = (index) => {
+    //     toggleTaken(id,index); // need to handel an index
+    // };
 
     const handleDelete = () => {
         if (window.confirm('Are you sure you want to delete this medication?')) {
@@ -28,25 +28,25 @@ function Medication({ medication, toggleTaken, onDelete }) {
                     <p className="times">Times:</p>
                     <ul className="times-list">
                         {times.map((time, index) => (
-                            <li key={index}>{time}
-                                <input
-                                    type="checkbox"
-                                    checked={taken[index]}
-                                    onChange={() => handleTakenChange(index)}
-                                />
-                            </li>
-                            // <li key={index}>{time}</li>
+                            // <li key={index}>{time}
+                            //     <input
+                            //         type="checkbox"
+                            //         checked={taken[index]}
+                            //         onChange={() => handleTakenChange(index)}
+                            //     />
+                            // </li>
+                            <li key={index}>{time}</li>
                         ))}
                     </ul>
                 </div>
             )}
-            {/*<div className="taken">*/}
-            {/*    <input*/}
-            {/*        type="checkbox"*/}
-            {/*        checked={taken}*/}
-            {/*        onChange={() => toggleTaken(id)}*/}
-            {/*    />*/}
-            {/*</div>*/}
+            <div className="taken">
+                <input
+                    type="checkbox"
+                    checked={taken}
+                    onChange={() => toggleTaken(id)}
+                />
+            </div>
             <div className="delete"> {/* New Column */}
                 <button onClick={handleDelete}>Delete</button>
             </div>
