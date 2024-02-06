e0b1ce3667b50fcc8e0694101306193397a8f3d4
kriskris79 <krzysztofkakolewski@googlemail.com>
2024-02-06 16:04:53 +0000

    preperation for authentication

--- medhelper/src/components/MedicationForm.js
+++ medhelper/src/components/MedicationForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
-import db from '../components/Firebase';
+import db from '../config/Firebase';
 
 function MedicationForm({ onCancel, onAddMedication }) {
     const [name, setName] = useState('');
