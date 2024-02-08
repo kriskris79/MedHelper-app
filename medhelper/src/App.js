a5e120e1a0c88eaa9517042c17b4c96aaf309ecf
kriskris79 <krzysztofkakolewski@googlemail.com>
2024-02-08 11:24:54 +0000

    Delete unwanted old version of the code.

--- medhelper/src/App.js
+++ medhelper/src/App.js
@@ -64,36 +64,3 @@ function App() {
 
 export default App;
 
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
-// import './scss/styles/_app.scss';
-// import "./scss/styles/_main.scss";
-// import MedicationList from './components/MedicationList';
-// import Register from './components/Register';
-// import Login from './components/Login';
-// import PasswordlessLogin from './components/PasswordlessLogin';
-//
-// function App() {
-//     return (
-//         <Router>
-//             <div className="app">
-//                 <nav>
-//                     <h1 className="title">My Medications List</h1>
-//                     <Link to="/login">Login</Link>
-//                     <Link to="/register">Register</Link>
-//                     <Link to="/medications">Medications</Link>
-//                 </nav>
-//                 <Routes>
-//                     <Route path="/register" element={<Register />} />
-//                     <Route path="/login" element={<Login />} />
-//                     <Route path="/passwordless-login" element={<PasswordlessLogin />} />
-//                     <Route path="/medications" element={<MedicationList />} />
-//                     <Route path="/" element={<Navigate replace to="/medications" />} />
-//                 </Routes>
-//             </div>
-//         </Router>
-//     );
-// }
-//
-// export default App;
